da83fa46ab842f5280e28805f5973e870bb30037
cccaaannn <can.kurt.aa@gmail.com>
2024-04-25 19:50:13 +0300

    gateway service

---
+++ sep-gateway-service/src/main/java/com/kurtcan/sepgatewayservice/SepGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kurtcan.sepgatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SepGatewayServiceApplication.class, args);
+	}
+
+}
