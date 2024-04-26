607f7f34ceb8342a5df5eff03406d2be21e6ef71
cccaaannn <can.kurt.aa@gmail.com>
2024-04-27 01:08:04 +0300

    product service

---
+++ sep-product-service/src/main/java/com/kurtcan/sepproductservice/SepProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kurtcan.sepproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SepProductServiceApplication.class, args);
+	}
+
+}
