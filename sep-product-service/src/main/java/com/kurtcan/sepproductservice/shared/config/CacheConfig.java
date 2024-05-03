d5d9c94dd8c8168fcb696971230a70ce5b72246b
cccaaannn <can.kurt.aa@gmail.com>
2024-05-03 22:23:26 +0300

    cache config

---
+++ sep-product-service/src/main/java/com/kurtcan/sepproductservice/shared/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package com.kurtcan.sepproductservice.shared.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+}
