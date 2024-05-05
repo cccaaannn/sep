670243b89d69bff9780cc5a33efb3d985c69c1fd
cccaaannn <can.kurt.aa@gmail.com>
2024-05-05 06:09:40 +0300

    refactor

---
+++ sep-product-service/src/main/java/com/kurtcan/sepproductservice/shared/constant/ProfileName.java
@@ -0,0 +1,7 @@
+package com.kurtcan.sepproductservice.shared.constant;
+
+public class ProfileName {
+    public static final String TEST = "test";
+    public static final String DEV = "dev";
+    public static final String PROD = "prod";
+}
---
+++ sep-product-service/src/main/java/com/kurtcan/sepproductservice/shared/constant/ServiceName.java
@@ -0,0 +1,5 @@
+package com.kurtcan.sepproductservice.shared.constant;
+
+public class ServiceName {
+    public static final String PAYMENT = "sep-payment-service";
+}
