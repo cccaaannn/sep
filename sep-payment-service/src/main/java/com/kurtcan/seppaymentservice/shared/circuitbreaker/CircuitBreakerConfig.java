0bb397fd25b98e1513469a25aa0fea4bc43b8c91
cccaaannn <can.kurt.aa@gmail.com>
2024-05-09 21:15:01 +0300

    circuitbreaker config

---
+++ sep-payment-service/src/main/java/com/kurtcan/seppaymentservice/shared/circuitbreaker/CircuitBreakerConfig.java
@@ -0,0 +1,20 @@
+package com.kurtcan.seppaymentservice.shared.circuitbreaker;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class CircuitBreakerConfig {
+
+    private final Resilience4JCircuitBreakerFactory circuitBreakerFactory;
+
+    @Bean(CircuitBreakerName.PRODUCT)
+    public CircuitBreaker productServiceCircuitBreaker() {
+        return circuitBreakerFactory.create(CircuitBreakerName.PRODUCT);
+    }
+
+}
---
+++ sep-payment-service/src/main/java/com/kurtcan/seppaymentservice/shared/circuitbreaker/CircuitBreakerName.java
@@ -0,0 +1,5 @@
+package com.kurtcan.seppaymentservice.shared.circuitbreaker;
+
+public class CircuitBreakerName {
+    public static final String PRODUCT = "circuitbreaker-product-service";
+}
