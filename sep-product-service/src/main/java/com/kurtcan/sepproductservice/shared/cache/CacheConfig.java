e53d2434696a5f82bfa09cf1ce7ba7829f6201af
cccaaannn <can.kurt.aa@gmail.com>
2024-05-25 05:42:43 +0300

    cache config updated

--- sep-product-service/src/main/java/com/kurtcan/sepproductservice/shared/cache/CacheConfig.java
+++ sep-product-service/src/main/java/com/kurtcan/sepproductservice/shared/cache/CacheConfig.java
@@ -1,9 +1,12 @@
 package com.kurtcan.sepproductservice.shared.cache;
 
+import com.kurtcan.sepproductservice.shared.constant.ProfileName;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @EnableCaching
 @Configuration
+@Profile("!" + ProfileName.TEST)
 public class CacheConfig {
 }
