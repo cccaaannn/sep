bd8fdac88fda3a6f71a8344520f0aa6743a6f72c
cccaaannn <can.kurt.aa@gmail.com>
2024-06-01 01:11:09 +0300

    circuitbreaker config

---
+++ sep-search-service/src/main/java/com/kurtcan/sepsearchservice/shared/circuitbreaker/CircuitBreakerConfig.java
@@ -0,0 +1,25 @@
+package com.kurtcan.sepsearchservice.shared.circuitbreaker;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class CircuitBreakerConfig {
+
+    private final Resilience4JCircuitBreakerFactory circuitBreakerFactory;
+
+    @Bean(CircuitBreakerName.PRODUCT)
+    public CircuitBreaker productServiceCircuitBreaker() {
+        return circuitBreakerFactory.create(CircuitBreakerName.PRODUCT);
+    }
+
+    @Bean(CircuitBreakerName.PAYMENT)
+    public CircuitBreaker paymentServiceCircuitBreaker() {
+        return circuitBreakerFactory.create(CircuitBreakerName.PAYMENT);
+    }
+
+}
---
+++ sep-search-service/src/main/java/com/kurtcan/sepsearchservice/shared/circuitbreaker/CircuitBreakerName.java
@@ -0,0 +1,6 @@
+package com.kurtcan.sepsearchservice.shared.circuitbreaker;
+
+public class CircuitBreakerName {
+    public static final String PRODUCT = "circuitbreaker-product-service";
+    public static final String PAYMENT = "circuitbreaker-payment-service";
+}
