ae1d6289315032e2c2f09d999306706002242a79
cccaaannn <can.kurt.aa@gmail.com>
2024-07-13 03:18:53 +0300

    constants

--- sep-search-service/src/main/java/com/kurtcan/sepsearchservice/shared/constant/ServiceName.java
+++ sep-search-service/src/main/java/com/kurtcan/sepsearchservice/shared/constant/ServiceName.java
@@ -3,4 +3,5 @@ package com.kurtcan.sepsearchservice.shared.constant;
 public class ServiceName {
     public static final String PRODUCT = "sep-product-service";
     public static final String PAYMENT = "sep-payment-service";
+    public static final String KEYCLOAK = "keycloak";
 }
