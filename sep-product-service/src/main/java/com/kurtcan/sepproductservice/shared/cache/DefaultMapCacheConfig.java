1a7bd4931168575264fa2997fce47cde18f7ebca
cccaaannn <can.kurt.aa@gmail.com>
2024-10-20 02:42:39 +0300

    refactor

--- sep-product-service/src/main/java/com/kurtcan/sepproductservice/shared/cache/DefaultMapCacheConfig.java
+++ sep-product-service/src/main/java/com/kurtcan/sepproductservice/shared/cache/DefaultMapCacheConfig.java
@@ -1,15 +1,12 @@
 package com.kurtcan.sepproductservice.shared.cache;
 
-import com.kurtcan.sepproductservice.shared.constant.ProfileName;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("!" + ProfileName.TEST)
 @ConditionalOnProperty(prefix = "redis", name = "enabled", havingValue = "false", matchIfMissing = true)
 public class DefaultMapCacheConfig {
 
