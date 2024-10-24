f3c829f610835522bb419e099cbf0fe1ecaca63d
cccaaannn <can.kurt.aa@gmail.com>
2024-10-25 01:51:19 +0300

    mapper config

---
+++ sep-aggregator-service/src/main/java/com/kurtcan/sepaggregatorservice/shared/mapper/ObjectMapperConfig.java
@@ -0,0 +1,34 @@
+package com.kurtcan.sepaggregatorservice.shared.mapper;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean(name = "objectMapper")
+    public ObjectMapper getObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.enable(JsonGenerator.Feature.IGNORE_UNKNOWN);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return objectMapper;
+    }
+
+    @Bean(name = "hazelcastObjectMapper")
+    public ObjectMapper getHazelcastObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.enable(JsonGenerator.Feature.IGNORE_UNKNOWN);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.activateDefaultTyping(objectMapper.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.EVERYTHING, JsonTypeInfo.As.PROPERTY);
+        return objectMapper;
+    }
+}
