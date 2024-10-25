96f0a461c69f6b529f8da3043f31ef370bcdbf58
cccaaannn <can.kurt.aa@gmail.com>
2024-10-26 00:29:11 +0300

    event helpers

---
+++ sep-aggregator-service/src/main/java/com/kurtcan/sepaggregatorservice/shared/event/DataEvent.java
@@ -0,0 +1,25 @@
+package com.kurtcan.sepaggregatorservice.shared.event;
+
+import com.kurtcan.sepaggregatorservice.shared.entity.DbEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DataEvent<T> {
+    private UUID id;
+    private T data;
+
+    public static <T> DataEvent<T> fromEntity(DbEntity dbEntity, T data) {
+        return DataEvent.<T>builder()
+                .id(dbEntity.getId())
+                .data(data)
+                .build();
+    }
+}
---
+++ sep-aggregator-service/src/main/java/com/kurtcan/sepaggregatorservice/shared/event/KafkaProperties.java
@@ -0,0 +1,27 @@
+package com.kurtcan.sepaggregatorservice.shared.event;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Data
+@Component
+@RefreshScope
+@ConfigurationProperties(prefix = "kafka")
+public class KafkaProperties {
+
+    @NestedConfigurationProperty
+    private List<String> bootstrapServers;
+
+    @NestedConfigurationProperty
+    private Consumer consumer;
+
+    @Data
+    public static class Consumer {
+        private String groupId;
+    }
+}
